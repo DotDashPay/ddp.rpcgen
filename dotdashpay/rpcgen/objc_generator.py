2c2dc201c9e0dc70c75f3ea98e42dbed7407c47d
Sean Arietta <sean.arietta@gmail.com>
2016-05-08 14:13:45 -0700

    Expanded functionality of objc_generator.
    Can now convert between protobuf type and a string representation of the type. Also added in a fixer for uncrustify's inability to correctly indent inline comments correctly

--- dotdashpay/rpcgen/objc_generator.py
+++ dotdashpay/rpcgen/objc_generator.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python
 
 from ddp_generator import DDPGenerator, to_camel_case
+from google.protobuf.descriptor_pb2 import FieldDescriptorProto
 import jsbeautifier
 import os
+import re
 from subprocess import Popen, PIPE, STDOUT
 
 UNCRUSTIFY_ROOT_DIR = "{}{}..{}..{}thirdparty{}uncrustify".format(os.path.dirname(os.path.realpath(__file__)),
@@ -13,10 +15,16 @@ UNCRUSTIFY_ROOT_DIR = "{}{}..{}..{}thirdparty{}uncrustify".format(os.path.dirnam
 UNCRUSTIFY_PATH = "{}{}src{}uncrustify".format(UNCRUSTIFY_ROOT_DIR, os.path.sep, os.path.sep)
 UNCRUSTIFY_CFG = "{}{}configs{}objc.cfg".format(UNCRUSTIFY_ROOT_DIR, os.path.sep, os.path.sep)
 
+re_inline_comment = re.compile(ur"(.*;) *(//.*)(\n)(\n*)")
+
 class ObjectiveCGenerator(DDPGenerator):
 
     def class_prefix(self):
         return "DDP"
+
+    def import_from_proto_file(self, proto_path):
+        filename = proto_path.split("/")[-1].replace(".proto", ".pbobjc.h")
+        return filename[0].upper() + filename[1:]
 
     def language(self):
         return "objc"
@@ -25,19 +33,46 @@ class ObjectiveCGenerator(DDPGenerator):
         if not os.path.isfile(UNCRUSTIFY_PATH):
             raise "Cannot beautify Objective-C code without uncrustify, which can be built via setup.sh"
 
-        pipe = Popen([UNCRUSTIFY_PATH, "-q", "-c", "{}".format(UNCRUSTIFY_CFG)],
+        pipe = Popen([UNCRUSTIFY_PATH, "-q", "-l", "oc", "-c", "{}".format(UNCRUSTIFY_CFG)],
                      stdout=PIPE, stdin=PIPE, stderr=STDOUT)
         output = pipe.communicate(input="{}\n".format(code))[0]
-        return output.decode()
+        intermediate = output.decode()
+
+        # Fix annoying bug that doesn't keep spacing between code and
+        # inline comment.
+        fixed = re.sub(re_inline_comment, u"\g<1> \g<2>\n", intermediate)
+        return fixed
+
+    def get_type_name(self, protobuf_type):
+        return {
+            FieldDescriptorProto.TYPE_BOOL: "BOOL",
+            FieldDescriptorProto.TYPE_BYTES: "NSData*",
+            FieldDescriptorProto.TYPE_DOUBLE: "double",
+            FieldDescriptorProto.TYPE_ENUM: "int32_t",
+            FieldDescriptorProto.TYPE_FIXED32: "",
+            FieldDescriptorProto.TYPE_FIXED64: "",
+            FieldDescriptorProto.TYPE_FLOAT: "float",
+            FieldDescriptorProto.TYPE_GROUP: "",
+            FieldDescriptorProto.TYPE_INT32: "int32_t",
+            FieldDescriptorProto.TYPE_INT64: "int64_t",
+            FieldDescriptorProto.TYPE_MESSAGE: "",
+            FieldDescriptorProto.TYPE_SFIXED32: "int32_t",
+            FieldDescriptorProto.TYPE_SFIXED64: "int64_t",
+            FieldDescriptorProto.TYPE_SINT32: "int32_t",
+            FieldDescriptorProto.TYPE_SINT64: "int64_t",
+            FieldDescriptorProto.TYPE_STRING: "NSString*",
+            FieldDescriptorProto.TYPE_UINT32: "uint32_t",
+            FieldDescriptorProto.TYPE_UINT64: "uint64_t",
+        }[protobuf_type]
 
     def recase(self, variable):
         return to_camel_case(variable)
 
     def api_header_name(self, service):
-        return "{}{}.h".format(self.class_prefix(), service.name)
+        return None#return "{}{}.h".format(self.class_prefix(), service.name)
 
     def api_source_name(self, service):
-        return None#"{}{}.m".format(self.class_prefix(), service.name)
+        return None#return "{}{}.m".format(self.class_prefix(), service.name)
 
     def simulator_header_name(self, services):
         return None
@@ -46,8 +81,7 @@ class ObjectiveCGenerator(DDPGenerator):
         return None
 
     def examples_source_name(self, method_name):
-        return None
-        #return "{}Example.{}.m".format(self.class_prefix(), method_name)
+        return "{}Example.{}.m".format(self.class_prefix(), method_name)
 
     def tests_source_name(self, method_name):
         return None
